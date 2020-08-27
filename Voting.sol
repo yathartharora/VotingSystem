e871cd789c8636334713b0927314f1dcfffeb1bf
Yatharth arora <yathartharora1999@gmail.com>
2020-08-27 07:07:07 +0530

    Update Voting.sol

--- Voting.sol
+++ Voting.sol
@@ -1,41 +1,77 @@
 pragma experimental ABIEncoderV2;
 pragma solidity ^0.6.0;
+import "./RegisterAsVoter.sol";
 
 contract Voting{
     
+    RegisterAsVoter globals;
+    // struct Voter{
+    //     string FirstName;
+    //     string LastName;
+    //     string AadharCardNumber;
+    //     uint Age;
+    //     string MobileNumber;
+    // }
     
-    struct Voter{
+    struct Candidate{
         string FirstName;
         string LastName;
         string AadharCardNumber;
         uint Age;
         string MobileNumber;
+        uint VotesApproved;
     }
     
-    Voter[] public voters;
+    //Voter[] public voters;
+    Candidate[] public candidates;
     address public manager;
+    mapping(address => bool) registeredascandidate;
+    mapping(address => bool) voted;
     
-    constructor () public{
-        manager = msg.sender;
+    
+    modifier restricted()
+    {
+        require(msg.sender==manager);
+        _;
     }
     
-    function register(string memory firstname,string memory lastname,string memory Aadharnumber,uint age,string memory mobilenumber) public{
-        Voter memory newVoter = Voter({
+    constructor (address _addr) public{
+        manager = msg.sender;
+        globals = RegisterAsVoter(_addr);
+    }
+    
+    // function registerAsVoter(string memory firstname,string memory lastname,string memory Aadharnumber,uint age,string memory mobilenumber) public{
+    //     Voter memory newVoter = Voter({
+    //         FirstName: firstname,
+    //         LastName: lastname,
+    //         AadharCardNumber: Aadharnumber,
+    //         Age: age,
+    //         MobileNumber: mobilenumber
+    //     });
+        
+    //     voters.push(newVoter);
+    //     registeredasvoter[msg.sender] = true;
+    // }
+    
+    function registerAsCandidate(string memory firstname,string memory lastname,string memory Aadharnumber,uint age,string memory mobilenumber) public{
+        Candidate memory newCandidate = Candidate({
             FirstName: firstname,
             LastName: lastname,
             AadharCardNumber: Aadharnumber,
             Age: age,
-            MobileNumber: mobilenumber
+            MobileNumber: mobilenumber,
+            VotesApproved: 0
         });
-        
-        voters.push(newVoter);
+        candidates.push(newCandidate);
     }
     
-    function display(uint index) public view returns(string memory, string memory){
-        return (
-            voters[index].FirstName,
-            voters[index].LastName
-            );
+    function Vote(uint index) public{
+        require(globals.registeredasvoter(msg.sender));
+        require(!voted[msg.sender]);
+        candidates[index].VotesApproved++;
+        voted[msg.sender] = true;
     }
+    
+    
     
 }
